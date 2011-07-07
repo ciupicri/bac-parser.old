794af058d005c200f3bf6714a2311b98f2fc9ced
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2011-07-08 00:19:13 +0300

    get_data_from_mainTable: group tr tags by hint

--- maintable.py
+++ maintable.py
@@ -1,3 +1,4 @@
+import itertools
 import lxml.html
 
 def get_mainTable(html):
@@ -9,13 +10,15 @@ def get_mainTable(html):
 
 def get_data_from_mainTable(main_table):
     L = []
-    for tr in main_table.xpath(r'''tr[@hint]'''):
-        if tr.find('script') is not None:
-            d = get_data_from_tr_with_script(tr)
-            L.append(d)
-        else:
-            d = get_data_from_tr_without_script(tr)
-            L.append(d)
+    get_hint = lambda tr: tr.attrib['hint'].strip().upper()
+    for hint, trs in itertools.groupby(main_table.xpath(r'''tr[@hint]'''), get_hint):
+        d = {}
+        for tr in trs:
+            if tr.find('script') is not None:
+                d.update(get_data_from_tr_with_script(tr))
+            else:
+                d.update(get_data_from_tr_without_script(tr))
+        L.append(d)
     return L
 
 def get_data_from_tr_with_script(tr):
