82a0d2f9ee2704d34dfeaff3665e8a453d96daa5
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2011-07-08 01:49:35 +0300

    Extract all the relevant data from tr elements

--- maintable.py
+++ maintable.py
@@ -12,7 +12,7 @@ def get_data_from_mainTable(main_table):
     L = []
     get_hint = lambda tr: tr.attrib['hint'].strip().upper()
     for hint, trs in itertools.groupby(main_table.xpath(r'''tr[@hint]'''), get_hint):
-        d = {}
+        d = {'nume': hint}
         for tr in trs:
             if tr.find('script') is not None:
                 d.update(get_data_from_tr_with_script(tr))
@@ -22,26 +22,34 @@ def get_data_from_mainTable(main_table):
     return L
 
 def get_data_from_tr_with_script(tr):
-    d = {}
-    d['scoala'] = tr.xpath('td[5]/a/text()')[0].strip()
-    d['judet'] = tr.xpath('td[6]/a/text()')[0].strip()
-    d['specializare'] = tr.xpath('td[9]/text()')[0].strip()
-    d['d1_nume'] = "limba si literatura romana"
-    d['d1_oral'] = tr.xpath('td[10]/text()')[0].strip()
-    d['d1_scris_final']= tr.xpath('td[13]/text()')[0].strip()
-    d['d0_nume'] = tr.xpath('td[14]/text()')[0].strip()
-    d['d2_nume'] = tr.xpath('td[15]/text()')[0].strip()
-    d['d2_oral'] = tr.xpath('td[16]/text()')[0].strip()
-    d['d3_nume'] = tr.xpath('td[17]/text()')[0].strip()
-    d['d4_nume'] = tr.xpath('td[18]/text()')[0].strip()
-    d['d5_nume'] = tr.xpath('td[19]/text()')[0].strip()
+    cols = (None, # Nr. crt.
+            None, # Nume... <script>
+            None, # Pozitia in ierarhie...
+            None, # Pozitia in ierarhie...
+            'scoala',
+            'judet',
+            'promotie_anterioara',
+            'forma_invatamant',
+            'specializare',
+            'd_romana_competente', 'd_romana_scris_nota', 'd_romana_scris_nota_contestatie', 'd_romana_scris_nota_finala',
+            'd_limba_materna_nume',
+            'd_limba_moderna_nume', 'd_limba_moderna_nota',
+            'd_profil_scris_nume',
+            'd_alegere_scris_nume',
+            'd_competente_digitale',
+            None, # LuatDePe_Bac...
+            None, # Luat_DePe_Bac...
+            )
+    d = {c: td.xpath('.//text()')[0].strip()
+            for c, td in zip(cols, tr.xpath('td')) if c}
     return d
 
 def get_data_from_tr_without_script(tr):
     d = {}
-    d['d0_oral'] = tr.xpath('td[1]/text()')[0].strip()
-    d['d0_scris_final'] = tr.xpath('td[4]/text()')[0].strip()
-    d['d3_scris_final'] = tr.xpath('td[7]/text()')[0].strip()
-    d['d4_scris_final'] = tr.xpath('td[10]/text()')[0].strip()
-    d['d5_scris_final'] = tr.xpath('td[13]/text()')[0].strip()
+    cols = ('d_limba_materna_competente', 'd_limba_materna_scris_nota', 'd_limba_materna_scris_nota_contestatie', 'd_limba_materna_scris_nota_finala',
+            'd_profil_scris_nota', 'd_profil_scris_nota_contestatie', 'd_profil_scris_nota_finala',
+            'd_alegere_scris_nota', 'd_alegere_nota_scris_contestatie', 'd_alegere_nota_scris_finala'
+            )
+    d = {c: td.xpath('.//text()')[0].strip()
+            for c, td in zip(cols, tr.xpath('td')) if c}
     return d
