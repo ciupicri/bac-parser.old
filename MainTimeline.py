c10723780dd0ba33c11bff49567f2baed6360756
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2011-07-07 17:38:12 +0300

    pythonification: remove this

--- MainTimeline.py
+++ MainTimeline.py
@@ -2,11 +2,11 @@
     _loc_2 = None
     _loc_1 = ged
     if (_loc_1 != null):
-        _loc_1 = this.s0(_loc_1, "0", "O")
-        _loc_1 = this.s0(_loc_1, "1", "l")
-        _loc_1 = this.s0(_loc_1, "5", "S")
-        _loc_1 = this.s0(_loc_1, "m", "s")
-        _loc_1 = this.s2(_loc_1)
+        _loc_1 = s0(_loc_1, "0", "O")
+        _loc_1 = s0(_loc_1, "1", "l")
+        _loc_1 = s0(_loc_1, "5", "S")
+        _loc_1 = s0(_loc_1, "m", "s")
+        _loc_1 = s2(_loc_1)
         #_loc_2 = ExternalInterface.call("function() { return window.top.location.href; }")
         #if (_loc_2 != null && _loc_2.indexOf("http://bacalaureat.edu.ro/") == 0):
         #    ExternalInterface.call("sdd", Base64.decode(_loc_1))
@@ -22,13 +22,13 @@ def s0(param1, param2, param3):
     return _loc_4
 
 def s1(param1, param2):
-    return this.s0(param1, param2.lower(), param2.upper())
+    return s0(param1, param2.lower(), param2.upper())
 
 def s2(param1):
     _loc_2 = param1
     _loc_3 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     _loc_4 = 0
     while (_loc_4 < _loc_3.length):
-        _loc_2 = this.s1(_loc_2, _loc_3[_loc_4])
+        _loc_2 = s1(_loc_2, _loc_3[_loc_4])
         _loc_4 += 1
     return _loc_2
