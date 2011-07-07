5f5819534f7ac9c698034d1cbc24ef592890b8ef
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2011-07-08 00:22:02 +0300

    WIP: get_data_from_tr_with/out_script
    Extract some data in get_data_from_tr_with_script and
    get_data_from_tr_without_script.

--- maintable.py
+++ maintable.py
@@ -23,8 +23,25 @@ def get_data_from_mainTable(main_table):
 
 def get_data_from_tr_with_script(tr):
     d = {}
+    d['scoala'] = tr.xpath('td[5]/a/text()')[0].strip()
+    d['judet'] = tr.xpath('td[6]/a/text()')[0].strip()
+    d['specializare'] = tr.xpath('td[9]/text()')[0].strip()
+    d['d1_nume'] = "limba si literatura romana"
+    d['d1_oral'] = tr.xpath('td[10]/text()')[0].strip()
+    d['d1_scris_final']= tr.xpath('td[13]/text()')[0].strip()
+    d['d0_nume'] = tr.xpath('td[14]/text()')[0].strip()
+    d['d2_nume'] = tr.xpath('td[15]/text()')[0].strip()
+    d['d2_oral'] = tr.xpath('td[16]/text()')[0].strip()
+    d['d3_nume'] = tr.xpath('td[17]/text()')[0].strip()
+    d['d4_nume'] = tr.xpath('td[18]/text()')[0].strip()
+    d['d5_nume'] = tr.xpath('td[19]/text()')[0].strip()
     return d
 
 def get_data_from_tr_without_script(tr):
     d = {}
+    d['d0_oral'] = tr.xpath('td[1]/text()')[0].strip()
+    d['d0_scris_final'] = tr.xpath('td[4]/text()')[0].strip()
+    d['d3_scris_final'] = tr.xpath('td[7]/text()')[0].strip()
+    d['d4_scris_final'] = tr.xpath('td[10]/text()')[0].strip()
+    d['d5_scris_final'] = tr.xpath('td[13]/text()')[0].strip()
     return d
