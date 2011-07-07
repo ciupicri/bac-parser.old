5a935b57c0adedbbbb6955631df6155d59331a76
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2011-07-07 19:57:20 +0300

    Extract mainTable

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+import lxml.etree
 import re
 import sys
 import MainTimeline
+import maintable
 
 ged_regex = re.compile(r'''function\s+ged\(\)\s*{\s*return\s+"([^"]*)"\s*;\s*}''')
 
@@ -9,6 +11,8 @@ f = open(sys.argv[1], 'rt')
 html = f.read()
 f.close()
 
+
 ged = ged_regex.search(html).group(1)
-data = MainTimeline.s3(ged).decode('base64')
-print data
+html = MainTimeline.s3(ged).decode('base64')
+main_table = maintable.get_mainTable(html)
+print lxml.etree.tostring(main_table, pretty_print=True)
---
+++ maintable.py
@@ -0,0 +1,8 @@
+import lxml.html
+
+def get_mainTable(html):
+    html = html[html.index('<HTML>'):]
+    html = unicode(html, 'utf-8')
+    doc = lxml.html.fromstring(html)
+    main_table = doc.xpath(r'''//table[@id="mainTable"]''')[0]
+    return main_table
