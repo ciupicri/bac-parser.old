3fdad128d553521bd91278131b7f716196e5b6d7
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2011-07-08 10:30:53 +0300

    Move get_data_from_file from main to utils

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
 #!/usr/bin/env python
 import logging
 import logging.config
-import lxml.etree
-import re
 import sys
-import MainTimeline
-import maintable
 
-ged_regex = re.compile(r'''function\s+ged\(\)\s*{\s*return\s+"([^"]*)"\s*;\s*}''')
-
-def get_data_from_file(f):
-    logger = logging.getLogger('get_data_from_file')
-    html = f.read()
-    ged = ged_regex.search(html).group(1)
-    html = MainTimeline.s3(ged).decode('base64')
-    main_table = maintable.get_mainTable(html)
-    if logger.isEnabledFor(logging.DEBUG):
-        logger.debug('main_table:\n' + \
-            lxml.etree.tostring(main_table, pretty_print=True))
-    return maintable.get_data_from_mainTable(main_table)
+from utils import get_data_from_file
 
 def main():
     with open(sys.argv[1], 'rt') as f:
