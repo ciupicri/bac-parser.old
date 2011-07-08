e69677c7d0cab5a76a2c920674ce9acabcd24829
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2011-07-08 04:26:15 +0300

    main: extracted get_data_from_file function

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ import maintable
 
 ged_regex = re.compile(r'''function\s+ged\(\)\s*{\s*return\s+"([^"]*)"\s*;\s*}''')
 
-def main():
-    f = open(sys.argv[1], 'rt')
+def get_data_from_file(f):
     html = f.read()
-    f.close()
-
     ged = ged_regex.search(html).group(1)
     html = MainTimeline.s3(ged).decode('base64')
     main_table = maintable.get_mainTable(html)
     with open('/tmp/main_table.xml', 'wt') as f:
         f.write(lxml.etree.tostring(main_table, pretty_print=True))
-    data = maintable.get_data_from_mainTable(main_table)
-    for i in data:
-        print i
-        print '='*72
+    return maintable.get_data_from_mainTable(main_table)
+
+def main():
+    with open(sys.argv[1], 'rt') as f:
+        for i in get_data_from_file(f):
+            print i
+            print '='*72
 
 if __name__ == '__main__':
     main()
