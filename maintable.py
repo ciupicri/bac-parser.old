bcd2b4734322817ce518838306d00ca829d5296f
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2011-07-08 04:04:46 +0300

    maintable: get_data_from_tr* -> get_data_from_tr

--- maintable.py
+++ maintable.py
@@ -3,6 +3,34 @@ import lxml.html
 
 from elev import Elev
 
+
+TR_SCRIPT_COLS = (
+    None, # Nr. crt.
+    None, # Nume... <script>
+    None, # Pozitia in ierarhie...
+    None, # Pozitia in ierarhie...
+    'scoala',
+    'judet',
+    'promotie_anterioara',
+    'forma_invatamant',
+    'specializare',
+    'd_romana_competente', 'd_romana_scris_nota', 'd_romana_scris_nota_contestatie', 'd_romana_scris_nota_finala',
+    'd_limba_materna_nume',
+    'd_limba_moderna_nume', 'd_limba_moderna_nota',
+    'd_profil_scris_nume',
+    'd_alegere_scris_nume',
+    'd_competente_digitale',
+    None, # LuatDePe_Bac...
+    None, # Luat_DePe_Bac...
+    )
+
+TR_WITHOUT_SCRIPT_COLS = (
+    'd_limba_materna_competente', 'd_limba_materna_scris_nota', 'd_limba_materna_scris_nota_contestatie', 'd_limba_materna_scris_nota_finala',
+    'd_profil_scris_nota', 'd_profil_scris_nota_contestatie', 'd_profil_scris_nota_finala',
+    'd_alegere_scris_nota', 'd_alegere_nota_scris_contestatie', 'd_alegere_nota_scris_finala'
+    )
+
+
 def get_mainTable(html):
     html = html[html.index('<HTML>'):]
     html = unicode(html, 'utf-8')
@@ -17,41 +45,12 @@ def get_data_from_mainTable(main_table):
         d = {'nume': hint}
         for tr in trs:
             if tr.find('script') is not None:
-                d.update(get_data_from_tr_with_script(tr))
+                d.update(get_data_from_tr(tr, TR_SCRIPT_COLS))
             else:
-                d.update(get_data_from_tr_without_script(tr))
+                d.update(get_data_from_tr(tr, TR_WITHOUT_SCRIPT_COLS))
         L.append(Elev(**d))
     return L
 
-def get_data_from_tr_with_script(tr):
-    cols = (None, # Nr. crt.
-            None, # Nume... <script>
-            None, # Pozitia in ierarhie...
-            None, # Pozitia in ierarhie...
-            'scoala',
-            'judet',
-            'promotie_anterioara',
-            'forma_invatamant',
-            'specializare',
-            'd_romana_competente', 'd_romana_scris_nota', 'd_romana_scris_nota_contestatie', 'd_romana_scris_nota_finala',
-            'd_limba_materna_nume',
-            'd_limba_moderna_nume', 'd_limba_moderna_nota',
-            'd_profil_scris_nume',
-            'd_alegere_scris_nume',
-            'd_competente_digitale',
-            None, # LuatDePe_Bac...
-            None, # Luat_DePe_Bac...
-            )
-    d = {c: td.xpath('.//text()')[0].strip()
-            for c, td in zip(cols, tr.xpath('td')) if c}
-    return d
-
-def get_data_from_tr_without_script(tr):
-    d = {}
-    cols = ('d_limba_materna_competente', 'd_limba_materna_scris_nota', 'd_limba_materna_scris_nota_contestatie', 'd_limba_materna_scris_nota_finala',
-            'd_profil_scris_nota', 'd_profil_scris_nota_contestatie', 'd_profil_scris_nota_finala',
-            'd_alegere_scris_nota', 'd_alegere_nota_scris_contestatie', 'd_alegere_nota_scris_finala'
-            )
-    d = {c: td.xpath('.//text()')[0].strip()
-            for c, td in zip(cols, tr.xpath('td')) if c}
-    return d
+def get_data_from_tr(tr, cols):
+    return {c: td.xpath('.//text()')[0].strip()
+                for c, td in zip(cols, tr.xpath('td')) if c}
