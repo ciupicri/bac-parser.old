21670d20d9670bc2c3f2da62d2aea98734904352
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2011-07-09 16:27:28 +0300

    main.py: add support for compressed files

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python
 import logging
 import logging.config
+import os.path
 import sys
+from gzip import GzipFile
+from bz2 import BZ2File
+from lzma import LZMAFile
 
 from utils import get_data_from_file
 
+COMPRESSED_FILE_CLASSES = {'.gz': GzipFile,
+                           '.bz2': BZ2File,
+                           '.xz': LZMAFile}
+
+def open_compressed_file(filename):
+    ext = os.path.splitext(filename)[1]
+    f = COMPRESSED_FILE_CLASSES.get(ext, file)(filename)
+    dir(f) # workaround for https://bugzilla.redhat.com/show_bug.cgi?id=720111
+    return f
+
 def main():
     for filename in sys.argv[1:]:
-        with open(filename, 'rt') as f:
+        logging.info("Extracting from %s" % (filename,))
+        with open_compressed_file(filename) as f:
             for i in get_data_from_file(f):
                 print i
                 print '#'*72
