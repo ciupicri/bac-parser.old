e1fce295e287985401233c597a07e23c9d5af2f8
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2011-07-09 23:06:52 +0300

    bac2010parser.get_data_from_file: analyze file line by line
    This makes it possible to extract from multiple concatenated files.
    Good for stuff like this: tar xjfO /tmp/pages.tar.bz2 | ./main.py -

--- bac2010parser/__init__.py
+++ bac2010parser/__init__.py
@@ -11,11 +11,14 @@ def get_data_from_file(f):
     global js_ged_regex
 
     logger = logging.getLogger('get_data_from_file')
-    html = f.read()
-    ged = js_ged_regex.search(html).group(1)
-    html = MainTimeline.s3(ged).decode('base64')
-    main_table = maintable.get_mainTable(html)
-    if logger.isEnabledFor(logging.DEBUG):
-        logger.debug('main_table:\n' + \
-            lxml.etree.tostring(main_table, pretty_print=True))
-    return maintable.get_data_from_mainTable(main_table)
+    for line in f:
+        ged = js_ged_regex.search(line)
+        if not ged:
+            continue
+        html = MainTimeline.s3(ged.group(1)).decode('base64')
+        main_table = maintable.get_mainTable(html)
+        if logger.isEnabledFor(logging.DEBUG):
+            logger.debug('main_table:\n' + \
+                lxml.etree.tostring(main_table, pretty_print=True))
+        for i in maintable.get_data_from_mainTable(main_table):
+            yield i
