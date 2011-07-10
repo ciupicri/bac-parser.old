0938594234b7874df4837c059a1d10995fc33723
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2011-07-10 21:33:05 +0300

    bac2010parser.maintable: add main

--- bac2010parser/maintable.py
+++ bac2010parser/maintable.py
@@ -1,5 +1,7 @@
 import logging
+import lxml.etree
 import lxml.html
+import sys
 
 def get_mainTable(html):
     html = html[html.index('<HTML>'):]
@@ -11,3 +13,12 @@ def get_mainTable(html):
 def get_data_from_tr(tr, cols):
     return {c: td.xpath('.//text()')[0].replace('&nbsp', '').strip()
                 for c, td in zip(cols, tr.xpath('td')) if c}
+
+def main(f):
+    with f:
+        html = f.read()
+        main_table = get_mainTable(html)
+        print lxml.etree.tostring(main_table, pretty_print=True)
+
+if __name__ == '__main__':
+    main(sys.stdin)
