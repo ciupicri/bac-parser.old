8f6d27a28cf46352dd097701d79ecf9a6eef2828
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2011-07-14 04:06:08 +0300

    Add bacparser.rezultate2006 (initial commit)

---
+++ bacparser/rezultate2006/__init__.py
@@ -0,0 +1,45 @@
+import logging
+
+from .elev import Elev
+from ..utils import grouper
+from ..maintable import get_data_from_tr
+
+TR1_COLS = (
+    None, # Nr. crt.
+    'nume',
+    None, # Pozitia in ierarhie...
+    None, # Pozitia in ierarhie...
+    'scoala',
+    'judet',
+    'promotie_anterioara',
+    'forma_invatamant',
+    'specializare',
+    'd_romana_oral_nota',
+    'd_romana_scris_nota', 'd_romana_scris_nota_contestatie', 'd_romana_scris_nota_finala',
+    None, # col span...
+    'd_limba_moderna_nume', 'd_limba_moderna_nota',
+    'd_profil_scris_nume',
+    'd_alegere_aria_curiculara_nume',
+    'd_alegere_alte_arii_curiculare_nume',
+    None, # media
+    'rezultat_final',
+    )
+
+TR2_COLS = (
+    'd_limba_materna_oral_nota',
+    'd_limba_materna_scris_nota', 'd_limba_materna_scris_nota_contestatie', 'd_limba_materna_scris_nota_finala',
+    'd_profil_scris_nota', 'd_profil_scris_nota_contestatie', 'd_profil_scris_nota_finala',
+    'd_alegere_aria_curiculara_nota', 'd_alegere_aria_curiculara_nota_contestatie', 'd_alegere_aria_curiculara_nota_finala',
+    'd_alegere_alte_arii_curiculare_nota', 'd_alegere_alte_arii_curiculare_nota_contestatie', 'd_alegere_alte_arii_curiculare_nota_finala',
+    )
+
+def get_elev_from_mainTable(main_table):
+    logger = logging.getLogger('bac2010parser.rezultate.get_elev_from_mainTable')
+    for trs in grouper(2, main_table.xpath(r'''tr[@hint]''')):
+        d = get_data_from_tr(trs[0], TR1_COLS)
+        d.update(get_data_from_tr(trs[1], TR2_COLS))
+
+        elev = Elev(**d)
+        if logger.isEnabledFor(logging.INFO):
+            logger.info("extracted %s" % (elev,))
+        yield elev
